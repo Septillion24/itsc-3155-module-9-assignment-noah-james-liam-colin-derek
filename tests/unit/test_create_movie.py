eb71094758c63d2aebe2da4c3fb52add0ad1d8b6
Liam Porter <lappqp@gmail.com>
2024-03-20 22:42:43 -0400

    Adding tests for movie creation

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,15 @@
-# TODO: Feature 2
+from flask import app
+from src.repositories.movie_repository import get_movie_repository
+
+def test_create_movie():
+    movie_repo = get_movie_repository()
+    movie_repo.clear_db()
+    movie_name = 'The Matrix'
+    director_name = 'Jarold'
+    rating = 5
+
+    movie = movie_repo.create_movie(movie_name, director_name, rating)
+
+    assert movie.title == movie_name
+    assert movie.director == director_name
+    assert movie.rating == rating
