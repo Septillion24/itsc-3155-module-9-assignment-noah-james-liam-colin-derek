6224b25c20df1564f3f00164d47c71e8fa86e44c
Septillion24 <noahgrosh@gmail.com>
2024-03-20 20:22:57 -0400

    completed assignment

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,10 @@
-# TODO: Feature 4
+def test_get_movie_by_id(self):
+    # Success condition
+    movie = self.repo.create_movie("Test Movie", "Test Director", 5)
+    retrieved_movie = self.repo.get_movie_by_id(movie.id)
+    self.assertEqual(movie, retrieved_movie)
+
+    # Fail condition: 
+    non_existing_id = -1
+    with self.assertRaises(Exception): #idk what exception you raise
+        self.repo.get_movie_by_id(non_existing_id)
