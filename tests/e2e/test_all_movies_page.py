9576250c0beb0f67b98539e7eff9e81e9d73bfb2
Antisocial-Gamer <james83701@gmail.com>
2024-03-20 21:23:28 -0400

    first tests up

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,9 @@
 # TODO: Feature 1
+from app import app
+
+def test_all_movies_blank():
+    test_app = app.test_client()
+    response = test_app.get('/movies')
+    assert 200 == response.status_code
+
+def 
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,9 @@
-# TODO: Feature 1
+from src.repositories.movie_repository import get_movie_repository
+from app import app
+
+def test_movie_add():
+    movies = get_movie_repository()
+    movies.clear_db()
+    movies.create_movie("Movie", "Dir", 5)
+    assert movies.get_movie_by_title("Movie").director == "Dir"
+    movies.clear_db()
